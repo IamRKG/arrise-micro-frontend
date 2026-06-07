ce42a381153ec351b27ce58920c03c88b505da52
IamRKG <radhacreative@gmail.com>
2026-06-07 05:52:01 -0500

    Optimize: dynamic import Vue in VueWrapper to reduce Host bundle size

--- apps/host/src/VueWrapper.jsx
+++ apps/host/src/VueWrapper.jsx
@@ -1,26 +1,25 @@
-                                                                                           
-  import React, { useRef, useEffect, memo } from "react";                                
-  import { createApp } from "vue";                                                         
-                                                                                         
-  const VueWrapper = memo(({ vueComponent }) => {                                          
-    const containerRef = useRef(null);                                                   
-    const appRef = useRef(null);                                                           
-                                                                                         
-    useEffect(() => {                                                                      
-      if (containerRef.current && vueComponent) {                                        
-        appRef.current = createApp(vueComponent);                                          
-        appRef.current.mount(containerRef.current);
-      }                                                                                    
-                                                                                         
-      return () => {                                                                       
-        if (appRef.current) {                
-          appRef.current.unmount();                                                        
-        }                                                                                  
-      };                                      
-    }, [vueComponent]);                                                                    
-                                                                                         
-    return <div ref={containerRef} />;                                                     
-  });                                      
-                                                                                           
-  export default VueWrapper;                                                             
-                             
+  import React, { useRef, useEffect, memo } from "react";                                                
+                                                                                                       
+  const VueWrapper = memo(({ vueComponent }) => {                                                        
+    const containerRef = useRef(null);
+    const appRef = useRef(null);                                                                         
+                                                                                                       
+    useEffect(() => {
+      if (containerRef.current && vueComponent) {                                                        
+        import("vue").then(({ createApp }) => {
+          appRef.current = createApp(vueComponent);                                                      
+          appRef.current.mount(containerRef.current);                                                  
+        });                                   
+      }                                   
+
+      return () => {                                                                                     
+        if (appRef.current) {
+          appRef.current.unmount();                                                                      
+        }                                                                                              
+      };
+    }, [vueComponent]);                       
+                                          
+    return <div ref={containerRef} />;
+  });                                                                                                    
+   
+  export default VueWrapper;  
